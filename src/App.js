572cbbdd64f12a756d2f9d1fa0108581e2d74435
kaurnavjeet <kaurnavjeet09@gmail.com>
2018-07-11 03:08:35 -0700

    Assignment Routing: completed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
 import './App.css';
 import Landing from './components/Landing'
 import Library from './components/Library'
+import Album from './components/Album'
 
 class App extends Component {
   render() {
@@ -22,6 +23,7 @@ class App extends Component {
         <main>
           <Route exact path='/' component={Landing} />
           <Route path='/library' component={Library} />
+          <Route path='/album' component={Album} />
         </main>
       </div>
     );
