a362fb38a29ce5c459b581df726523f61dfe3c76
kaurnavjeet <kaurnavjeet09@gmail.com>
2018-07-13 14:33:39 -0700

    Checkpoint Player Bar Buttons: completed

---
+++ src/components/PlayerBar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+class PlayerBar extends Component {
+  render() {
+    return (
+      <section className="player-bar">
+
+        <section id="button">
+          <button id="previous" onClick={this.props.handlePrevClick}>
+            <span className="ion-md-rewind"></span>
+          </button>
+          <button id="play-pause" onClick={this.props.handleSongClick}>
+            <span className={this.props.isPlaying ? "ion-md-pause" : "ion-md-play"}></span>
+          </button>
+          <button id="next">
+            <span className="ion-md-fastforward"></span>
+          </button>
+        </section>
+
+        <section id="time-control">
+          <div className="current-time">-:--</div>
+          <input type="range" className="seek-bar" value="0" />
+          <div className="total-time">-:--</div>
+        </section>
+
+        <section id="volume-control">
+          <div className="ion-md-volume-low"></div>
+          <input type="range" className="seek-bar" value="80" />
+          <div className="ion-md-volume-high"></div>
+        </section>
+
+      </section>
+    )
+  }
+}
+
+export default PlayerBar
