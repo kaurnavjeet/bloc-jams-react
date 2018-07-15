717fb08b09f5830bdd2052f37900e8176beff3b0
kaurnavjeet <kaurnavjeet09@gmail.com>
2018-07-14 22:19:59 -0700

    CSS: WIP2

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Landing = () => (
   <section className="landing">
-    <div className="title">
+    <div className="head-title">
       <div className="music-title">
         <h1 className="hero-title">Turn the music up</h1>
         <div className="slider left"></div>
@@ -13,17 +13,17 @@ const Landing = () => (
 
 
     <section className="selling-point">
-      <div className="point">
-        <h2 className="point-title">Choose your music</h2>
-        <p className="point-description">The world is full of music; why should you listen to music that someone else chose?</p>
+      <div className="point-left">
+        <h2 className="title">Choose your music</h2>
+        <p className="description">The world is full of music; why should you listen to music that someone else chose?</p>
       </div>
-      <div className="point">
-        <h2 className="point-title">Unlimited, streaming, ad-free</h2>
-        <p className="point-description">No arbitrary limits. No distractions.</p>
-      </div>
-      <div className="point">
-        <h2 className="point-title">Mobile enabled</h2>
-        <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platform.</p>
+      <div className="point-center">
+        <h2 className="title">Unlimited, streaming, ad-free</h2>
+        <p className="description">No arbitrary limits. No distractions.</p>
+       </div>
+        <div className="point-right">
+        <h2 className="title">Mobile enabled</h2>
+        <p className="description">Listen to your music on the go. This streaming service is available on all mobile platform.</p>
       </div>
     </section>
   </section>
