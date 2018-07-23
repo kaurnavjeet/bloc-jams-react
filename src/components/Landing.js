c93a6f032276e11669294a2d98aa47405a43b9bb
kaurnavjeet <kaurnavjeet09@gmail.com>
2018-07-22 20:01:55 -0700

    Fixed Landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,7 +6,7 @@ const Landing = () => (
 
     <section className="selling-point">
       <div className="point">
-        <i class="fas fa-music fa-2x" />
+        <i className="fas fa-music fa-2x" />
         <h2 className="point-title">Choose your music</h2>
         <p className="point-description">
           The world is full of music; why should you listen to music that
@@ -14,14 +14,14 @@ const Landing = () => (
         </p>
       </div>
       <div className="point">
-        <i class="fas fa-headphones-alt fa-2x" />
+        <i className="fas fa-headphones-alt fa-2x" />
         <h2 className="point-title">Unlimited, streaming, ad-free</h2>
         <p className="point-description">
           No arbitrary limits. No distractions.
         </p>
       </div>
       <div className="point">
-        <i class="fas fa-mobile-alt fa-2x" />
+        <i className="fas fa-mobile-alt fa-2x" />
         <h2 className="point-title">Mobile enabled</h2>
         <p className="point-description">
           Listen to your music on the go. This streaming service is available on
